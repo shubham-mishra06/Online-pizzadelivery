888922eb46a3c18148c3274df33e0d559f51cccd
shubham <shu7388@gmail.com>
2022-04-11 15:00:20 +0530

    first commit

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Copyright © 2022 Designed by <a href="https://github.com/shubham-mishra06" target="_blank" rel="noopener noreferrer">@shubham</a></p>
+</div>
